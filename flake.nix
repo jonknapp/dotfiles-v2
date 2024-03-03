be67213c689cae253d5d5aeeac3b3bd8fed417d4
Jonathan Knapp <jon@coffeeandcode.com>
2024-03-03 16:32:21 -0500

    Add custom applications from remote flakes

--- flake.nix
+++ flake.nix
@@ -2,15 +2,23 @@
   description = "Home Manager configurations";
 
   inputs = {
+    git-remote-open = {
+      url = "github:CoffeeAndCode/git-remote-open";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    mdquote = {
+      url = "github:CoffeeAndCode/mdquote";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-caffeine.url = "github:nixos/nixpkgs/9f7d9a55cc9960c029b006444e64e8dfa54a578e";
   };
 
-  outputs = inputs @ { home-manager, nixpkgs, nixpkgs-caffeine, ... }:
+  outputs = inputs @ { home-manager, nixpkgs, ... }:
     let
       overlays = system: import ./overlays { inherit inputs system; };
       hmConfig = import ./hm-config-builder.nix { inherit home-manager overlays nixpkgs; };
@@ -23,6 +31,12 @@
           modules = [
             ./profiles/cli.nix
             ./profiles/desktop-full.nix
+            ({ pkgs, ... }: {
+              home.packages = [
+                inputs.git-remote-open.defaultPackage.${pkgs.system}
+                inputs.mdquote.defaultPackage.${pkgs.system}
+              ];
+            })
           ];
         };
     in
