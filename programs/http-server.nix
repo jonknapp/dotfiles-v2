fbe7ff4934da590fc99d96af098f6d4e44db0d1e
Jonathan Knapp <jon@coffeeandcode.com>
2024-03-25 21:49:00 -0400

    Refactor custom types config to not need xdg config file

--- programs/http-server.nix
+++ programs/http-server.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }:
 
 let
+  customTypesConfg = pkgs.writeText "custom-types" ''
+    types {
+      application/manifest+json webmanifest;
+    }
+  '';
   http-server = pkgs.writeScriptBin "http-server" ''
     #!${pkgs.stdenv.shell}
 
@@ -11,9 +16,7 @@ let
       fi
       echo "Simple static server started at http://localhost:8000 from $source_path"
       docker run -p 8000:80 --rm \
-        -v "$HOME/${
-          config.xdg.configFile."http-server/custom-types.conf".target
-        }":/etc/nginx/conf.d/custom-types.conf:ro \
+        -v "${customTypesConfg}":/etc/nginx/conf.d/custom-types.conf:ro \
         -v "$(cd "$source_path" || exit; echo "$PWD")":/usr/share/nginx/html:ro \
         nginx:alpine
     }
@@ -23,10 +26,4 @@ let
 in
 {
   home.packages = [ http-server ];
-
-  xdg.configFile."http-server/custom-types.conf".text = ''
-    types {
-      application/manifest+json webmanifest;
-    }
-  '';
 }
